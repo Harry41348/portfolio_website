6fa8350d4e053770cd097053b05bba105132149b
Harry Wijnschenk <hvalentinow@gmail.com>
2021-02-25 20:30:20 +0000

    Implemented landing page
    Landing page has been implemented for mobile.

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet"> 
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Mukta:wght@700&display=swap" rel="stylesheet"> 
         <link rel="stylesheet" type="text/css" href="css/style.css">
         <title>Harry W</title>
     </head>
@@ -13,17 +14,20 @@
         <header class="header">
             <div class="header__home">
                 <h1 class="header__home--primary">Harry Wijnschenk</h1>
-                <ol>
-                    <li>Software Engineering</li>
-                    <li>Designer</li>
-                </ol>
-                <button class="header__home--about">
-                    <span class="circle" aria-hidden="true">
-                        <span class="icon arrow"></span>
-                    </span>
-                    <span class="button-text">About Me</span>
-                </button>
-            </div>
+                <ul class="header__home--list">
+                    <li class="header__home--item">Web Developer</li>
+                    <span class="header__home--seperator">/</span>
+                    <li class="header__home--item">Student</li>
+                </ul>
+                <div class="button__container">
+                    <button class="home__button">
+                        <span class="home__button--circle" aria-hidden="true">
+                            <span class="icon arrow"></span>
+                        </span>
+                        <span class="home__button--text">About Me</span>
+                    </button>
+                </div>
+            </div>  
         </header>
     </body>
 </html>
