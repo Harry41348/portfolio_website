a9e3eae0c4ada46acc78fe6ef3e9b967002cc0bf
Harry Wijnschenk <hvalentinow@gmail.com>
2021-02-25 22:06:42 +0000

    Responsive landing page

--- index.php
+++ index.php
@@ -4,8 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Mukta:wght@700&display=swap" rel="stylesheet"> 
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&family=Mukta:wght@700&display=swap" rel="stylesheet"> 
         <link rel="stylesheet" type="text/css" href="css/style.css">
         <title>Harry W</title>
     </head>
@@ -15,17 +14,12 @@
             <div class="header__home">
                 <h1 class="header__home--primary">Harry Wijnschenk</h1>
                 <ul class="header__home--list">
-                    <li class="header__home--item">Web Developer</li>
+                    <li class="header__home--item">Software Developer</li>
                     <span class="header__home--seperator">/</span>
                     <li class="header__home--item">Student</li>
                 </ul>
-                <div class="button__container">
-                    <button class="home__button">
-                        <span class="home__button--circle" aria-hidden="true">
-                            <span class="icon arrow"></span>
-                        </span>
-                        <span class="home__button--text">About Me</span>
-                    </button>
+                <div class="center">
+                    <a href="#" class="header__home--button">About me</a>
                 </div>
             </div>  
         </header>
