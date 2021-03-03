537281c1aed2d2cb9956ca800f66442d6417cc87
Harry Wijnschenk <hvalentinow@gmail.com>
2021-03-03 21:28:45 +0000

    Portfolio experience developed
    Experience section has been marked up and styled

--- pages/about.php
+++ pages/about.php
@@ -25,9 +25,65 @@
     <p class="paragraph">As a person, I work effectively whether as part of a team, or as an individual. I am a 
       natural problem solver, motivated and hard-working person with a desire to achieve the best out of life.</p>
   </section>
+
   <section class="about__experience">
     <h2 class="sub-heading">Experience</h2>
+
+    <!-- Vava Project -->
+    <div class="experience">
+      <div class="experience__title">
+        <h3 class="experience__title--name">Vava Rush</h3>
+        <p class="experience__title--date">January - February 2021</p>
+      </div>
+      <div class="experience__description">
+        <div class="experience__description--tags">
+          <span class="tag tag__orange">HTML5</span>
+          <span class="tag tag__blue">CSS</span>
+          <span class="tag tag__purple">PHP</span>          
+        </div>
+        <p class="experience__description--text">My first official website. I developed this for a friend as a photography portfolio. 
+          The website uses a database to store images and using PHP to display all the projects and images. This project improved my 
+          understanding of development, project analysis, time management and problem solving skills.</p>
+      </div>
+    </div>
+
+    <!-- Django Project -->
+    <div class="experience">
+      <div class="experience__title">
+        <h3 class="experience__title--name">Django project</h3>
+        <p class="experience__title--date">November - December 2020</p>
+      </div>
+      <div class="experience__description">
+        <div class="experience__description--tags">
+          <span class="tag tag__orange">HTML5</span>
+          <span class="tag tag__purple">Bootstrap</span>
+          <span class="tag tag__green">Django</span>
+        </div>
+        <p class="experience__description--text">For one of my job applications, I had to develop a project in Django which was my 
+          introduction to the MVT framework. Now with my free time, I am reading the Tango with Django book to get a better understanding 
+          of the framework and plan to make some bigger projects in the coming months.
+        </p>
+      </div>
+    </div>
+
+    <!-- Rush Game -->
+    <div class="experience">
+      <div class="experience__title">
+        <h3 class="experience__title--name">Rush</h3>
+        <p class="experience__title--date">March - October 2017</p>
+      </div>
+      <div class="experience__description">
+        <div class="experience__description--tags">
+          <span class="tag tag__grey">Unity</span>
+          <span class="tag tag__purple">C#</span>
+        </div>
+        <p class="experience__description--text">My first project I ever took on was a game called Rush. Briefly featured on the 
+        play store. The objective was simple, the player controls a circle which could move up or down, and the aim was to reach 
+        the end of the level without being stopped by obstacles.</p>
+      </div>
+    </div>
   </section>
+  
   <section class="about__skills">
     <h2 class="sub-heading">Skills</h2>
   </section>
