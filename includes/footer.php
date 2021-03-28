22a2d0a8ce2e84d33ca4ce170dd91065bbc6d9cb
Harry Wijnschenk <hvalentinow@gmail.com>
2021-03-28 16:17:46 +0100

    Footer modified

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,15 @@
 <footer class="footer" id="footer">
-  <ul class="footer__list">
-    <li><a href="https://github.com/Harry41348" target="_blank"><i class="fab fa-github"></i></a></li>
-    <li><a href="https://www.linkedin.com/in/harry-wij/" target="_blank"><i class="fab fa-linkedin"></i></a></li>
-  </ul>
+  <div class="footer__left">
+    <ul class="footer__list">
+      <li><a href="https://github.com/Harry41348" target="_blank"><i class="fab fa-github"></i></a></li>
+      <li><a href="https://www.linkedin.com/in/harry-wij/" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+    </ul>
+  </div>
+  <div class="footer__right">
+    <h4 class="footer__contact--text">Let's get in touch!</h4>
+    <div class="full-width"><a href="#" class="footer__contact--link">Contact</a></div>
+    <a href="mailto:hvalentinow@gmail.com" class="footer__contact--email">hvalentinow@gmail.com</a>
+  </div>
 </footer>
 </div>
 </body>
